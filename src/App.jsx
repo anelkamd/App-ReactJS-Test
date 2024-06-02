43ab500ae78e8e35875cc023e14546c658ec6010
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-02 09:51:55 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react"
+
 function App() {
 
-  let count = 0;
+  const [ count , setCount] = useState(0)
+
+  const increment = () => {
+    setCount( count + 1)
+  }
 
   return <>
     <p>Compteur : {count}</p>
+    <button onClick={increment}>Incrementer</button>
 
   </>
 }
