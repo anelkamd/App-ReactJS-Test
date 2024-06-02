7478e6ce133f44a6a6c82842f58977437021af03
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-02 09:53:46 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,14 @@ function App() {
   const increment = () => {
     setCount( count + 1)
   }
+  const decrement = () => {
+    setCount( count - 1)
+  }
 
   return <>
     <p>Compteur : {count}</p>
     <button onClick={increment}>Incrementer</button>
+    <button onClick={decrement}>decrementer</button>
 
   </>
 }
