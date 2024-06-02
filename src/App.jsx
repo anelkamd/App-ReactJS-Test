876378395d55d79b855d8d828f1c7b00985c4016
Anelka MD <anelkabagalaneofficiel@gmail.com>
2024-06-02 10:09:40 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 function App() {
 
-  const [ count , setCount] = useState(0)
+  /*const [ count , setCount] = useState(0)
 
   const increment = () => {
     setCount( count + 1)
@@ -10,12 +10,24 @@ function App() {
   const decrement = () => {
     setCount( count - 1)
   }
+  */
+
+  // Dans le cadre d'un obje //
+
+  const [ person , setperson] = useState({
+    firstname : 'Anelka',
+    Lastname : 'MD',
+    age : 22
+  })
+  const increment = () => {
+    setperson(...person.firstname, person.age + 1)
+  }
+
+  
 
   return <>
-    <p>Compteur : {count}</p>
+    <p>Age de : {person.firstname}</p>
     <button onClick={increment}>Incrementer</button>
-    <button onClick={decrement}>decrementer</button>
-
   </>
 }
 
